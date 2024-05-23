7f6a9651b1c3d070decb01c20f34efb0d2e3fdc1
Ahmed <fyotol@gmail.com>
2024-05-23 11:55:44 -0700

    22

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <div className="fixed bottom-0 flex justify-center h-12 w-full bg-white z-50 border-t-2 border-gray-300 shadow-md">
+      <div className="fixed bottom-0 flex justify-center h-14 w-full bg-white z-50 border-t-2 border-gray-300 shadow-md">
         <div className="container flex items-center justify-between w-full h-full text-xl">
           <img className="w-7 h-7 rounded-full" src="/me.jpg" alt="Error" />
           <i className="fa-brands fa-facebook-messenger"></i>
