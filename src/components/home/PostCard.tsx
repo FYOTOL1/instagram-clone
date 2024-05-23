cfe4d4d08abff3f4b181bb7fd23bbeb718bb175e
Ahmed <fyotol@gmail.com>
2024-05-22 19:24:35 -0700

    Test

--- src/components/home/PostCard.tsx
+++ src/components/home/PostCard.tsx
@@ -3,7 +3,12 @@ import { TPosts } from "@customTypes/Posts.ts";
 const PostCard = ({ id, username, avatar, likes }: TPosts) => {
   return (
     <>
-      <div className="w-full max-w-[500px] outline outline-1 h-[600px]"></div>
+      <div className="w-full max-w-[500px] outline outline-1 h-[600px]">
+        {likes}
+        {id}
+        {username}
+        {avatar}
+      </div>
     </>
   );
 };
