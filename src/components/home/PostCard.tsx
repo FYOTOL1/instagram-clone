81b5c76152d39cd7c414a419c1a7f2d6388f6d16
Ahmed <fyotol@gmail.com>
2024-05-23 07:36:42 -0700

    12

--- src/components/home/PostCard.tsx
+++ src/components/home/PostCard.tsx
@@ -10,14 +10,14 @@ const PostCard = ({ id, username, avatar, likes, date }: TPosts) => {
         className="relative w-full max-w-[600px] outline outline-1 h-[700px]"
       >
         <img
-          className="h-full w-full object-scale-down bg-[#111]"
+          className="h-full w-full object-cover bg-[#111]"
           src={avatar}
           alt="Error"
         />
 
         <div className="container absolute top-0 bottom-0 flex flex-col justify-between w-full">
           <div className="flex justify-between items-center py-2">
-            <button className="flex items-center justify-center text-sm h-7 w-16 text-white outline outline-1 rounded-md">
+            <button className="flex items-center justify-center text-sm h-7 w-16 text-white outline outline-1 rounded-md focus:opacity-80 hover:opacity-80">
               <p>Follow</p>
             </button>
             <div className="flex items-center flex-row-reverse gap-2 text-white">
